e9b5460f5fb62e0c00666281eaf586d01ae1c6d5
guptaji1008 <132588314+guptaji1008@users.noreply.github.com>
2023-08-29 12:12:10 +0530

    os module

--- index.js
+++ index.js
@@ -1,20 +1,16 @@
-// Node core modules :-
+const os = require("os")
 
-const fs = require("fs")
+// console.log(os.arch())
 
-// fs.writeFileSync('read.txt', 'hello, my name is Aman')
-// fs.writeFileSync('read.txt', 'hello, my name is Aman Kumar Gupta')
-// fs.appendFileSync('read.txt', '. Aman is a hard working guy')
-// const buf_data = fs.readFileSync("read.txt")
-// const buf_data = fs.readFileSync("readWrite.txt", "utf-8")
-// console.log(buf_data)
-// Node js includes an additional datatype called buffer
-// (Not available in browser javascript)
-// buffer is only used to store binary data,
-// from reading from a file or recieving packets from network.
-{/* <Buffer 68 65 6c 6c 6f 2c 20 6d 79 20 6e 61 6d 65 20 69 73 20 41 6d 61 6e 20 4b 75 6d 61 72 20 47 75 70 74 61 2e 20 41 6d 61 6e 20 69 73 20 61 20 68 61 72 64 ... 12 more bytes> */}
+const freeMemory = os.freemem()
 
-// const org_data = buf_data.toString()
-// console.log(org_data)
+console.log(`${freeMemory/1024/1024/1024}`)
 
-// fs.renameSync('read.txt', 'readWrite.txt')
+const totalMemory = os.totalmem()
+
+console.log(`${totalMemory/1024/1024/1024}`)
+
+console.log(os.hostname())
+console.log(os.type())
+console.log(os.tmpdir())
+console.log(os.platform())
