9463b61fc99866513e67a972173823673f3ac197
guptaji1008 <132588314+guptaji1008@users.noreply.github.com>
2023-08-29 14:26:10 +0530

    npm modules

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-// How to create and export our own modules in node js :-
+// NPM (node package manager) :-
 
-// const oper = require('./oper')
-const { add, sub } = require('./oper')
 
-// console.log(oper.add(45, 97))
-// console.log(oper.sub(45, 23))
-console.log(add(45, 97))
-console.log(sub(45, 23))
-// console.log(oper)
+import chalk from 'chalk'
+import validator from 'validator'
+
+// console.log(chalk.red("Aman Kumar Gupta"))
+// console.log(chalk.green.underline.inverse("Aman Kumar Gupta"))
+// console.log(chalk.green.underline("Aman Kumar Gupta"))
+
+
+const res = validator.isEmail('cric.amankumar@gmail.com')
+
+res===true?console.log(chalk.green.underline.inverse(res)):console.log(chalk.red.underline.inverse(res))
 
