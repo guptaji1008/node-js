b817b0c2d3ad8dc9c4beb3022cbfa3330e5641a1
guptaji1008 <132588314+guptaji1008@users.noreply.github.com>
2023-08-29 15:54:03 +0530

    nodemon

--- index.js
+++ index.js
@@ -1,15 +1,2 @@
-// NPM (node package manager) :-
-
-
-import chalk from 'chalk'
-import validator from 'validator'
-
-// console.log(chalk.red("Aman Kumar Gupta"))
-// console.log(chalk.green.underline.inverse("Aman Kumar Gupta"))
-// console.log(chalk.green.underline("Aman Kumar Gupta"))
-
-
-const res = validator.isEmail('cric.amankumar@gmail.com')
-
-res===true?console.log(chalk.green.underline.inverse(res)):console.log(chalk.red.underline.inverse(res))
-
+console.log("aman")
+console.log("kumar")
